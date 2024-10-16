00af62f09f98c301706fa37e299720b6f8907720
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-10-16 18:31:18 -0500

    feat: repository fn for creating new tasks

--- infrastructure/driven-adapters/task/src/main/java/co/com/bancolombia/task/TaskRepositoryAdapter.java
+++ infrastructure/driven-adapters/task/src/main/java/co/com/bancolombia/task/TaskRepositoryAdapter.java
@@ -5,15 +5,26 @@ import co.com.bancolombia.model.task.constants.Constants;
 import co.com.bancolombia.model.task.gateways.TaskRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class TaskRepositoryAdapter implements TaskRepository {
 
-    private final List<Task> tasks = List.copyOf(Constants.initialTasks);
+    List<Task> tasks = new ArrayList<>(Constants.initialTasks);
+
+    private Integer counter = this.tasks.size();
 
     @Override
     public List<Task> findAll() {
         return this.tasks;
+    }
+
+    @Override
+    public Task create(Task task) {
+        this.counter++;
+        Task newTask = new Task(this.counter, task.title(), task.description());
+        this.tasks.add(newTask);
+        return newTask;
     }
 }
